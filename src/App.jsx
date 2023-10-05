6c65999c5fbe7a59f181389b73f8563044464cb7
Maxim Khanukaev <Andreymaxim387@gmail.com>
2023-10-05 12:48:17 +0300

    Подготовил файл app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
 import { useState } from 'react'
 import './App.css'
 
+// Вошел в аккаунт? 
+// - да - показать страницу
+// - нет - страница SignIn. Внизу: вы не зарегестрированы? - signUp
+// после нажатия кнопки signUp 'сохранить', перекидывает на войти
+// Вошел в аккаунт? 
+// - да - показать страницу
 export default function App() {
-
+  
   return (
     <>
-    <h1>Hi</h1>
+    
+    </>
+  )
+}
+
+function LogIn() {
+  
+  return(
+    <>
+    
+    </>
+  )
+}
+
+function SignIn() {
+  
+  return(
+    <>
+    
+    </>
+  )
+}
+
+function Input() {
+  
+  return(
+    <>
+    
+    </>
+  )
+}
+
+function Verification() {
+  
+  return(
+    <>
+    
     </>
   )
 }
