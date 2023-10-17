7471b79b8fb24ea9310c2c4dd2850068c869f9ea
Maxim Khanukaev <Andreymaxim387@gmail.com>
2023-10-17 21:41:25 +0300

    Готов макет дизайна

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,11 @@ export default function App() {
   const [ isSignIn, setIsSignIn ] = useState(false)
   const [ content, setContent ] = useState('')
   const [ isSignUp, setIsSignUp ] = useState(false)
+  const [ accountStatus, setAccountStatus ] = useState(true)
 
   const handleChange = () => {
-    setIsSignUp(true)
+    // setIsSignUp(true)
+    setAccountStatus(false)
   }
 
   useEffect( () => {
@@ -28,12 +30,13 @@ export default function App() {
       setContent(<Page/>)
     } else if(!isSignIn){
       setContent(<SignIn setIsSignIn={setIsSignIn} handleChange={handleChange} />)
+      // setContent(<SignUp setIsSignIn={setIsSignIn} handleChange={handleChange} />)
     } 
-    if (!isSignIn && isSignUp) {
-      setContent(<SignUp />)
+    if (!isSignIn && !accountStatus) {
+      setContent(<SignUp setAccountStatus={setAccountStatus}/>)
     }
 
-  }, [isSignIn, isSignUp] )
+  }, [isSignIn, accountStatus] )
 
   return (
     <>
@@ -50,7 +53,6 @@ function SignIn(props) {
   const [ stateOfPassword, setStateOfPassword ] = useState('password')
 
   
-
   const handleInputChange = (e) => {
     const {id, value} = e.target;
     if (id === 'userEmail') {
@@ -81,19 +83,19 @@ function SignIn(props) {
     }
   }
   return(
-    <div className='signIn__container'>
+    <div className='signInСontainer'>
 
-      <div className="textBlock">
+      <div className="textBlockSignIn">
         <h2>Войдите в аккаунт</h2>
 
         <div className="Inputes">
 
-          <form className="emailForm">
+          <form>
             <h4>Введите email</h4>
             <input className='emailinput'type="email"placeholder='Email'id='userEmail'onChange={(e) => handleInputChange(e)}value={props.userEmail}/>          
           </form>
 
-          <form className="passwordForm">
+          <form>
             <h4>Введите пароль</h4>
               <input className='passwordInput' type={stateOfPassword}placeholder='Пароль'id='userPassword'onChange={(e) => handleInputChange(e)}value={props.userPassword}/>
             <div className="checkboxContainer">
@@ -101,25 +103,26 @@ function SignIn(props) {
               <label htmlFor="showPassword">Показать пароль</label>
             </div>
           </form>
+
         </div>
 
         <button onClick={verification} className='signIn'>Войти в аккаунт</button>
-        <a href='#'onClick={props.handleChange} className="notAccount">У вас нет аккаунта?</a>
+        <a href='#SignUp'onClick={props.handleChange} className="notAccount">У вас нет аккаунта?</a>
       </div>
 
-      {/* <div className="imgBlock"> */}
-        <img src="https://images.unsplash.com/photo-1544963151-fb47c1a06478?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80"/>
-      {/* </div> */}
+        <img src="https://images.unsplash.com/photo-1544963151-fb47c1a06478?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80" className='imgSignIn'/>
     </div>
   )
 }
 
-function SignUp() {
-  const [dataName, setDataName] = useState('')
-  const [dataEmail, setDataEmail] = useState('')
-  const [dataPassword, setDataPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-  const [content, setContent] = useState ('')
+function SignUp(props) {
+  const [ dataName, setDataName ] = useState('')
+  const [ dataEmail, setDataEmail ] = useState('')
+  const [ dataPassword, setDataPassword ] = useState('')
+  const [ confirmPassword, setConfirmPassword ] = useState('')
+  const [ content, setContent ] = useState ('')
+  const [ stateOfPassword, setStateOfPassword ] = useState('password')
+  const [ passwordComplexity, setPasswordComplexity ] = useState('')
 
   const handleInputChange = (e) => {
     const {value, id} = e.target;
@@ -137,33 +140,115 @@ function SignUp() {
     }
   }
 
-    useEffect( () => {
-      dataPassword !== confirmPassword ? setContent('Пароли не совпадают') : setContent('✔️')
-    }, [dataPassword, confirmPassword] )
-
-    const signUp = () => {
+  // useEffect( () => {
+  // }, [dataPassword, confirmPassword] )
+  
+  const signUp = () => {
+    if (dataPassword !== confirmPassword) {
+      alert('Пароли не совпадают')
+    } else {
       localStorage.setItem( 'name', dataName )
       localStorage.setItem( 'email', dataEmail )
       localStorage.setItem( 'password', dataPassword )
+      props.setAccountStatus(true)
     }
 
+    // localStorage.setItem( 'name', dataName )
+    // localStorage.setItem( 'email', dataEmail )
+    // localStorage.setItem( 'password', dataPassword )
+    // props.setAccountStatus(true)
+    // dataPassword !== confirmPassword ? setContent('Пароли не совпадают') : setContent('✔️')
+    // dataPassword !== confirmPassword ? alert('Пароли не совпадают') : ''
+  }
+  const viewPassword = () => {
+    if (stateOfPassword === 'password') {
+      setStateOfPassword('text')
+    } else if (stateOfPassword === 'text') {
+      setStateOfPassword('password')
+    }
+    }
+  const onChangePassword = (e) => {
+    const {value, id} = e.target
+    if (id === 'dataPassword') {
+      setDataPassword(value)
+
+      if (dataPassword.length < 4) {
+        setPasswordComplexity(
+          <>
+          <hr className='easyPassword'/>
+          <hr className='defaultPassword'/> 
+          <hr className='defaultPassword'/> 
+          </>)
+      }
+      if (dataPassword.length > 4 ) {
+        setPasswordComplexity(
+          <>
+          <hr className='mediumPassword'/>
+          <hr className='mediumPassword'/>
+          <hr className='defaultPassword'/> 
+          </>)
+      }
+      if (dataPassword.length > 8 ) {
+        setPasswordComplexity(
+          <>
+          <hr className='strongPassword'/> 
+          <hr className='strongPassword'/> 
+          <hr className='strongPassword'/> 
+          </>)
+      }
+
+    }
+    if (id === 'confirmPassword') {
+      setConfirmPassword(value)
+    }
+  }
+
   return(
-    <>
-      <label htmlFor="dataName">Введите имя</label> <br />
-      <input type="text" placeholder='Александр' id="dataName" onChange={handleInputChange}/> <br />
-      
-      <label htmlFor="dataEmail">Введите email</label> <br />
-      <input type="email" placeholder='Email' id="dataEmail" onChange={handleInputChange}/> <br />
+    <div className="signUpContainer"> 
+      <img src="https://images.unsplash.com/photo-1682905926517-6be3768e29f0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80" className='imgSignUp'/>
 
-      <label htmlFor="dataPassword">Введите пароль</label> <br />
-      <input type="password" placeholder='Пароль' id="dataPassword" onChange={handleInputChange}/> <br />
+      <div className="textBlockSignUp">
+        <h2>Зарегистрируйтесь</h2>
 
-      <label htmlFor="confirmPassword">Повторите пароль</label> <br />
-      <input type="password" placeholder='Подтвердите пароль' id="confirmPassword" onChange={handleInputChange}/> <br />
-      {content}
-      <button onClick={signUp}>Зарегистрироваться</button>
+        <form>
+          <label htmlFor="dataName">Введите имя</label> <br />
+          <input type="text" placeholder='Имя' id="dataName" onChange={handleInputChange}/> <br />
+        </form>  
+        
+        <form>
+          <label htmlFor="dataEmail">Введите email</label> <br />
+          <input type="email" placeholder='Email' id="dataEmail" onChange={handleInputChange}/> <br />
+        </form>
 
-    </>
+        <form>
+          <label htmlFor="dataPassword">Введите пароль</label> <br />
+          <input type={stateOfPassword} placeholder='Пароль' id="dataPassword" onChange={onChangePassword}/> <br />
+
+          <div className="allAboutPassword">
+            <p>Сложность пароля</p>
+            <div className='passwordComplexityContainer'>
+              {passwordComplexity}
+            </div>
+          </div>
+        </form>
+
+        <form>
+          <label htmlFor="confirmPassword">Повторите пароль</label> <br />
+          <input type={stateOfPassword} placeholder='Подтвердите пароль' id="confirmPassword" onChange={onChangePassword}/> <br />
+          <div className="checkboxContainer">
+            <input type="checkbox" className='showPassword' id='showPassword' onClick={viewPassword}/>
+            <label htmlFor="showPassword">Показать пароль</label>
+          </div>
+        </form>
+
+
+
+          
+
+          {/* {content} */}
+          <button onClick={signUp} className='signUp'>Зарегистрироваться</button>
+      </div>
+    </div>
   )
 }
 
