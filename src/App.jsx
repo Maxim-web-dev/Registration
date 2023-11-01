8fd4a0729869b39a0504fb5053a1c54b03c6c1a7
Maxim Khanukaev <Andreymaxim387@gmail.com>
2023-11-01 14:38:28 +0300

    Тест

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import {
 } from "react-router-dom";
 import { SignIn } from "./components/signIn/signIn";
 import { SignUp } from "./components/signUp/signUp";
-import Layout from "./layout/Layout";
+import Layout from "./Layout/Layout";
 import ErrorElement from "./ErrorElement";
 import StartPage from "./pages/StartPage";
 import HomePage from "./pages/HomePage";
 import RequireAuth from "./hoc/RequireAuth";
 import { useState } from "react";
-import Account from "./layout/Account";
+import Account from "./Layout/Account";
 
 export default function App() {
   const [user, setUser] = useState(false)
@@ -20,33 +20,20 @@ export default function App() {
 
   const router = createBrowserRouter(
     createRoutesFromElements(
-      // <Route path='/' element={<Layout />} errorElement={<ErrorElement />}>
-      //   <Route index element={<StartPage />} />
-
-      //   <Route path='home' element={
-      //     <RequireAuth user={user}>
-      //       <HomePage />
-      //     </RequireAuth>
-      //   } />
-      //   <Route path="account" element={<Account />} />
-      //   <Route path='account/signin' element={<SignIn setUser={setUser}/>} />
-      //   <Route path='/account/signin' element={<SignIn setUser={setUser}/>} />
-      //   {/* <Route path='account/signup' element={<SignUp />} /> */}
-      //   <Route path='*' element={<ErrorElement />} />
-
-      // </Route>
-
       <Route path="/" element={<Layout />} errorElement={<ErrorElement />}>
         <Route index element={<StartPage />}/>
-
+        
         <Route path='home' element={
             <RequireAuth>
               <HomePage />
             </RequireAuth>
           } />
+
         <Route path="account" element={<Account />} >
-          <Route path='signin' element={<SignIn setUser={setUser}/>} />
+          <Route index element={<SignIn setUser={setUser}/>} />
+          <Route path='signup' element={<SignUp/>} />
         </Route>
+
       </Route>
     )
   );
